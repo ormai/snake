4754f8ee103122e9dc495fbda64424923535c249
Mario D'Andrea <goffredo2004@gmail.com>
2024-02-18 16:04:31 +0100

    Restarting games doesn't cause SEGV anymore

--- src/screen.c
+++ src/screen.c
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "screen.h"
@@ -14,9 +15,9 @@ Screen *newScreen(void) {
   self->offset = (Point){(self->screenWidth - self->width * 2) / 2,
                          (self->screenHeight - self->height) / 2};
 
-  self->grid = calloc(self->width * self->height, sizeof(bool));
-  spawnOrb(self);
+  self->grid = calloc(self->width * self->height, sizeof(int));
 
+  self->difficulty = INCREMENTAL;
   return self;
 }
 
@@ -40,6 +41,63 @@ void initializeNcurses(void) {
   use_default_colors();
 }
 
+static const char *difficultyFmt[] = {"  incremental >", "   < easy >    ",
+                                      "  < medium >   ", "   < hard      "};
+bool welcome(Screen *self) {
+  static const char *fmt[] = {
+      "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓",
+      "┃                             _                         ┃",
+      "┃                            | |                        ┃",
+      "┃             ___ _ __   __ _| | _____   ___            ┃",
+      "┃            / __| '_ \\ / _` | |/ / _ \\ / __|           ┃",
+      "┃            \\__ \\ | | | (_| |   <  __/| (__            ┃",
+      "┃            |___/_| |_|\\__,_|_|\\_\\___(_)___|           ┃",
+      "┃                                                       ┃",
+      "┃                         Play ⏎                        ┃",
+      "┃                                                       ┃",
+      "┃              Difficulty: %s              ┃",
+      "┃                                                       ┃",
+      "┃                         Quit q                        ┃",
+      "┃                                                       ┃",
+      "┃         by Mario D'Andrea <https://ormai.dev>         ┃",
+      "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"};
+
+  const int fmtHeight = 16, fmtWidth = 57;
+  const Point begin = {self->offset.x + self->width - fmtWidth / 2,
+                       self->offset.y + self->height / 2 - fmtHeight / 2};
+
+  for (int y = begin.y, i = 0; y < begin.y + fmtHeight; ++y, ++i)
+    if (i == 10) // Plug in the difficulty
+      mvprintw(y, begin.x, fmt[i], difficultyFmt[self->difficulty]);
+    else
+      mvprintw(y, begin.x, "%s", fmt[i]);
+
+  while (true) {
+    switch (getch()) {
+    case '\n':
+    case 'p':
+      drawWalls(self);
+      return true;
+    case KEY_RIGHT: // increment difficulty
+      if (self->difficulty != HARD) {
+        ++self->difficulty;
+        mvprintw(begin.y + 10, begin.x, fmt[10],
+                 difficultyFmt[self->difficulty]);
+      }
+      break;
+    case KEY_LEFT: // decrement difficulty
+      if (self->difficulty != INCREMENTAL) {
+        --self->difficulty;
+        mvprintw(begin.y + 10, begin.x, fmt[10],
+                 difficultyFmt[self->difficulty]);
+      }
+      break;
+    case 'q':
+      return false;
+    }
+  }
+}
+
 // Take an x coordinate and transate it for the screen
 // This is due to the fact that thare a point on th escree is two character wide
 // This is just for the representation
@@ -57,9 +115,17 @@ static void drawPointWithColor(const Screen *self, const Point pos,
   attrset(COLOR_PAIR(7)); // Restore white color
 }
 
-bool borders(const Screen *self, const Snake *snake) {
-  return snake->head->pos.x <= self->width && snake->head->pos.x >= 0 &&
-         snake->head->pos.y <= self->height && snake->head->pos.y >= 0;
+bool insideBoundaries(const Screen *self, const Snake *snake) {
+  FILE *log = fopen("log", "a");
+  fprintf(log, "Head (%d), (%d), W %d, H %d\n", snake->head->pos.x, snake->head->pos.y, self->width, self->height);
+  fclose(log);
+  if (snake->head->pos.x < self->width && snake->head->pos.x > 0 &&
+      snake->head->pos.y < self->height && snake->head->pos.y > 0) {
+    return true;
+  }
+
+  drawPointWithColor(self, snake->head->pos, COLOR_RED);
+  return false;
 }
 
 void spawnOrb(Screen *self) {
@@ -72,23 +138,28 @@ void spawnOrb(Screen *self) {
 }
 
 void updateScore(const Screen *self, const unsigned score) {
-  mvprintw(self->offset.y - 2, self->offset.x - 1, "Score: %d", score);
+  mvprintw(self->offset.y - 2, self->offset.x, "Score: %d", score);
 }
 
 void drawWalls(const Screen *self) {
-  mvprintw(self->offset.y - 1, self->offset.x - 1, "╔");
-  mvprintw(self->offset.y - 1, self->width * 2 + self->offset.x + 1, "╗");
-  mvprintw(self->height + self->offset.y + 1, self->offset.x - 1, "╚");
-  mvprintw(self->height + self->offset.y + 1,
-           self->width * 2 + self->offset.x + 1, "╝");
+  erase();
 
-  for (int i = self->offset.x; i <= self->width * 2 + self->offset.x; ++i) {
-    mvprintw(self->offset.y - 1, i, "═");
-    mvprintw(self->height + self->offset.y + 1, i, "═");
+  const Point topLeftCorner = {self->offset.x, self->offset.y},
+              bottomRightCorner = {translate(self->width) + self->offset.x,
+                                   self->height + self->offset.y};
+
+  mvprintw(topLeftCorner.y - 1, topLeftCorner.x, "╔");
+  mvprintw(topLeftCorner.y - 1, bottomRightCorner.x + 2, "╗");
+  mvprintw(bottomRightCorner.y + 1, topLeftCorner.x, "╚");
+  mvprintw(bottomRightCorner.y + 1, bottomRightCorner.x + 2, "╝");
+
+  for (int i = topLeftCorner.x + 1; i <= bottomRightCorner.x + 1; ++i) {
+    mvprintw(topLeftCorner.y - 1, i, "═");
+    mvprintw(bottomRightCorner.y + 1, i, "═");
   }
-  for (int j = self->offset.y; j <= self->height + self->offset.y; ++j) {
-    mvprintw(j, self->offset.x - 1, "║");
-    mvprintw(j, self->width * 2 + self->offset.x + 1, "║");
+  for (int j = topLeftCorner.y; j <= bottomRightCorner.y; ++j) {
+    mvprintw(j, topLeftCorner.x, "║");
+    mvprintw(j, bottomRightCorner.x + 2, "║");
   }
 }
 
@@ -96,24 +167,24 @@ void draw(const Screen *self, const Snake *snake, const bool growing,
           const Node *oldTail) {
   // Draw the new head added by Snake::advance()
   drawPointWithColor(self, snake->head->pos, COLOR_GREEN);
-  // self->grid[snake->head->y * self->width + snake->head->x] = true;
+  self->grid[snake->head->pos.y * self->width + snake->head->pos.x] = true;
 
   // Cover the old tail with a blank if the Snake has not grown
   if (!growing) {
     mvprintw(oldTail->pos.y + self->offset.y,
              translate(oldTail->pos.x) + self->offset.x, "  ");
-    // self->grid[oldTail->y * self->width + oldTail->x] = false; // segfault
+    self->grid[oldTail->pos.y * self->width + oldTail->pos.x] = false;
   }
 }
 
-bool gameOver(Screen *self, Snake *snake, const Point collision) {
-  if (collision.x != -1 && collision.y != -1)
-    drawPointWithColor(self, collision, COLOR_RED);
+bool gameOver(Screen *self, Snake *snake, Point *collision) {
+  if (collision->x != -1 && collision->y != -1)
+    drawPointWithColor(self, *collision, COLOR_RED);
 
   // hide score count above the playing field
   mvhline(self->offset.y - 2, self->offset.x - 1, ' ', self->width);
 
-  const char *fmt[] = {
+  static const char *fmt[] = {
       "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓",
       "┃   _____                        _____                  ┃",
       "┃  |  __ \\                      |  _  |                 ┃",
@@ -124,28 +195,56 @@ bool gameOver(Screen *self, Snake *snake, const Point collision) {
       "┃                                                       ┃",
       "┃                  Your score was: %-4d                 ┃",
       "┃                                                       ┃",
+      "┃              Difficulty: %s              ┃",
+      "┃                                                       ┃",
       "┃                      Play again?                      ┃",
       "┃                          y/n                          ┃",
       "┃                                                       ┃",
       "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"};
 
-  for (int i = self->offset.y + self->height / 2 - 7, j = 0;
-       i < self->offset.y + self->height / 2 - 7 + 14; ++i, ++j)
-    mvprintw(i, self->offset.x + self->width - 28, fmt[j], snake->length);
+  const int fmtHeight = 16, fmtWidth = 57;
+  const Point begin = {self->offset.x + self->width - fmtWidth / 2,
+                       self->offset.y + self->height / 2 - fmtHeight / 2};
+
+  for (int y = begin.y, i = 0; y < begin.y + fmtHeight; ++y, ++i)
+    if (i == 8) // Plug in the score
+      mvprintw(y, begin.x, fmt[i], snake->length);
+    else if (i == 10) // Plug in the difficulty
+      mvprintw(y, begin.x, fmt[i], difficultyFmt[self->difficulty]);
+    else
+      mvprintw(y, begin.x, "%s", fmt[i]);
 
   while (true) {
     switch (getch()) {
+    case '\n':
     case 'y':
     case 'Y': {
       // Reset the game
-      Screen *oldSelf = self;
-      Snake *oldSnake = snake;
+      destroyScreen(self);
+      destroySnake(snake);
+
       self = newScreen();
       snake = newSnake((Point){self->width / 2, self->height / 2});
-      destroyScreen(oldSelf);
-      destroySnake(oldSnake);
+
+      drawWalls(self);
+      *collision = (Point){-1, -1};
+      spawnOrb(self);
       return true;
     }
+    case KEY_RIGHT: // increment difficulty
+      if (self->difficulty != HARD) {
+        ++self->difficulty;
+        mvprintw(begin.y + 10, begin.x, fmt[10],
+                 difficultyFmt[self->difficulty]);
+      }
+      break;
+    case KEY_LEFT: // decrement difficulty
+      if (self->difficulty != INCREMENTAL) {
+        --self->difficulty;
+        mvprintw(begin.y + 10, begin.x, fmt[10],
+                 difficultyFmt[self->difficulty]);
+      }
+      break;
     case 'n':
     case 'N':
     case 'q':
