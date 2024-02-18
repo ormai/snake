3c6625cd7c4351f66f5d864630e3f88ca88772a6
Mario D'Andrea <goffredo2004@gmail.com>
2024-02-18 21:13:16 +0100

    Add license and copyright

--- src/snake.h
+++ src/snake.h
@@ -1,3 +1,17 @@
+/* snake.h -- Interface of the class Snake
+ *
+ * Copyright (C) 2024  Mario D'Andrea <https://ormai.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details. */
+
 #ifndef SNAKE_H
 #define SNAKE_H
 
@@ -44,5 +58,3 @@ void grow(Snake *self, Node *oldTail);
 bool selfCollision(const Snake *self, Point *collision);
 
 #endif // !SNAKE_H
-
-// vi:ft=c
