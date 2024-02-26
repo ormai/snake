29c4ffa6f2f19b6a63a12499ad5815dc71ec7e1a
Mario D'Andrea <goffredo2004@gmail.com>
2024-02-26 16:42:31 +0100

    Snake head has now different color

--- src/snake.c
+++ src/snake.c
@@ -12,6 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details. */
 
+#include <stdbool.h>
 #include <stdlib.h>
 
 #include "snake.h"
